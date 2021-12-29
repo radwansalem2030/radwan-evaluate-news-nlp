e8cc1685f769faafdf1063273782703830510ea0
Radwan Salem <85997938+radwansalem2030@users.noreply.github.com>
2021-12-29 17:05:41 +0200

    First Edit before start

--- src/server/index.js
+++ src/server/index.js
@@ -14,8 +14,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8084, function () {
+    console.log('Example app listening on port 8084!')
 })
 
 app.get('/test', function (req, res) {
